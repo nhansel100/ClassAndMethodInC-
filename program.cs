98b7c1471673a888141cc444b845553c599b9476
nhansel100 <nhansel100>
2020-04-13 14:17:22 -0500

    Initial Commit

---
+++ program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Nate";
+            person.Introduce("Kaylee");
+        }
+    }
+}
